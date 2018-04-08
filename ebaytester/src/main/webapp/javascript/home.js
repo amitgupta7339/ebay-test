2cb4cd79b52cb1e6fba543dc829cbd4ef8d5e959
Amit Gupta <amitgupta7339@gmail.com>
2018-04-08 13:22:10 +0530

    done

--- ebaytester/src/main/webapp/javascript/home.js
+++
@@ -1,9 +0,0 @@
-function searchfunction(){
-  if($('#Category').val() == '')
-     {
-     }
-  else {
-	  localStorage.category_name=$('#Category').val();
-        window.location="http://localhost:5224/ebaytester/prod_list_1.html";
-     }
-};
